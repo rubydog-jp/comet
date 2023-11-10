e5c1945bc4a1f2c9a6289a8c0d72fc4e66795d7a
Rubydog <hirabizz@gmail.com>
2023-11-10 17:39:19 +0900

    add: intro pages #31

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -16,7 +16,7 @@ function Header() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/start"
+            to="/docs/intro"
           >
             Meet Comet
           </Link>
