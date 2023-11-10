73d0a07d06a2bee8d4bbb2c15fc0bd254e809ee0
rbdog <rbdog@rubydog.jp>
2023-11-10 13:26:03 +0900

    fix: error 404 #28

--- comet_ui/lib/src/logic/to_url_path.dart
+++ comet_ui/lib/src/logic/to_url_path.dart
@@ -1,8 +1,8 @@
 import 'package:comet/src/types/state.dart';
 
 String toUrlPath(UiState state) {
-  final shelfSeg = state.selectedShelf;
-  final bookSeg = state.selectedBook;
-  final pageSeg = state.selectedPage;
+  final shelfSeg = state.selectedShelf.urlSeg;
+  final bookSeg = state.selectedBook.urlSeg;
+  final pageSeg = state.selectedPage.urlSeg;
   return '/$shelfSeg/$bookSeg/$pageSeg';
 }
