468cf99dbbb119821b5c1f255af76ab9f2bc9846
rbdog <rbdog@rubydog.jp>
2023-11-12 14:58:16 +0900

    fix: light mode color #34

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -9,7 +9,10 @@ import styles from "./index.module.css";
 function Header() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header
+      className={clsx("hero hero--primary", styles.heroBanner)}
+      style={{ backgroundColor: "var(--home-header)" }}
+    >
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -27,7 +30,6 @@ function Header() {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={`Home`} description="Comet Home Page">
       <Header />
