8d045eecb1563b6bcbfcf3cbfe41c5b6aa72738a
rbdog <rbdog@rubydog.jp>
2023-11-16 12:38:30 +0900

    disable: transition animation #35

--- comet_ui/lib/src/widgets/routes.dart
+++ comet_ui/lib/src/widgets/routes.dart
@@ -1,3 +1,4 @@
+import 'package:comet/src/widgets/page_builder.dart';
 import 'package:go_router/go_router.dart';
 import 'package:comet/src/logic/to_ui_state.dart';
 import 'package:comet/src/types/inline_widget.dart';
@@ -7,22 +8,23 @@ import 'package:comet/src/viewer.dart';
 class CometViewerRoute extends GoRoute {
   CometViewerRoute._({
     required super.path,
-    required super.builder,
+    required super.pageBuilder,
   });
 
   factory CometViewerRoute({
+    required String pathPattern,
     required CometGenData cometData,
     required CometInlineWidgets inlineWidgets,
   }) {
     return CometViewerRoute._(
-      path: cometPagePath,
-      builder: (_, state) {
-        return CometViewer(
+      path: pathPattern,
+      pageBuilder: (_, state) => noTransitionAnimatiobnPage(
+        child: CometViewer(
           pathParams: state.pathParameters,
           cometData: cometData,
           inlineWidgets: inlineWidgets,
-        );
-      },
+        ),
+      ),
     );
   }
 }
@@ -33,37 +35,22 @@ List<GoRoute> cometRoutes({
 }) {
   return [
     // URL: /shelf
-    CometViewerRoute._(
-      path: cometShelfPath,
-      builder: (_, state) {
-        return CometViewer(
-          pathParams: state.pathParameters,
-          cometData: cometData,
-          inlineWidgets: inlineWidgets,
-        );
-      },
+    CometViewerRoute(
+      pathPattern: cometShelfPath,
+      cometData: cometData,
+      inlineWidgets: inlineWidgets,
     ),
     // URL: /shelf/book
-    CometViewerRoute._(
-      path: cometBookPath,
-      builder: (_, state) {
-        return CometViewer(
-          pathParams: state.pathParameters,
-          cometData: cometData,
-          inlineWidgets: inlineWidgets,
-        );
-      },
+    CometViewerRoute(
+      pathPattern: cometBookPath,
+      cometData: cometData,
+      inlineWidgets: inlineWidgets,
     ),
     // URL: /shelf/book/page
-    CometViewerRoute._(
-      path: cometPagePath,
-      builder: (_, state) {
-        return CometViewer(
-          pathParams: state.pathParameters,
-          cometData: cometData,
-          inlineWidgets: inlineWidgets,
-        );
-      },
+    CometViewerRoute(
+      pathPattern: cometPagePath,
+      cometData: cometData,
+      inlineWidgets: inlineWidgets,
     ),
   ];
 }
