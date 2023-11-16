20adb45b46c9acad98fbcf055c7ad8f0e0e719a7
Rubydog <hirabizz@gmail.com>
2023-11-16 13:36:10 +0900

    update: upgrade command #29

--- comet_cli/lib/commands/upgrade.dart
+++ comet_cli/lib/commands/upgrade.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 
+import 'package:comet_cli/config.dart';
+import 'package:comet_cli/utils/progress_star.dart';
+
+/// process of upgrade
 Future<void> upgrade() async {
+  ProgressStar.start('upgrade $version --> latest');
+
   final process = await Process.start(
     'dart',
     [
@@ -12,4 +18,9 @@ Future<void> upgrade() async {
   );
 
   await process.exitCode;
+
+  ProgressStar.stop(msg: 'Completed.');
+  print('');
+  print('  \$ comet --version');
+  print('');
 }
